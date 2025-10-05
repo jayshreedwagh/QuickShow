14fefc581a00fde2a48f8e8a402bc9c24a41b9e7
jayshreedwagh <jayshreepatil5930@gmail.com>
2025-10-05 23:25:10 +0530

    Loading button

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -6,6 +6,7 @@ import { Heart, Plane, PlayCircle, PlayCircleIcon, StarIcon } from 'lucide-react
 import timeFormat from '../lib/timeFormat'
 import DateSelect from '../components/DateSelect'
 import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 const MovieDetails = () => {
   const navigate=useNavigate()
@@ -14,10 +15,13 @@ const MovieDetails = () => {
 
   const getShow=async()=>{
     const show=dummyShowsData.find(show=>show._id===id)
-    setShow({
+    if(show){
+      setShow({
       movie:show,
       dateTime:dummyDateTimeData
     })
+    }
+    
   }
   useEffect(()=>{
     getShow()
@@ -76,7 +80,7 @@ const MovieDetails = () => {
 
       </div>
     </div>
-  ):<div>Loading...</div>
+  ):<Loading/>
 }
 
 export default MovieDetails
