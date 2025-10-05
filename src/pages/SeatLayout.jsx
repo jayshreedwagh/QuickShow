b8d768f5145128b4965dcd39d3d2136a973a6ae9
jayshreedwagh <jayshreepatil5930@gmail.com>
2025-10-06 00:31:34 +0530

    seats added with id's

--- src/pages/SeatLayout.jsx
+++ src/pages/SeatLayout.jsx
@@ -5,11 +5,16 @@ import Loading from '../components/Loading'
 import { ClockIcon } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import BlurCircle from '../components/BlurCircle'
+import toast from 'react-hot-toast'
+
 
 const SeatLayout = () => {
+
+  const groupRows=[["A","B"],["C","D"],["E","F"],["G","H"],["I","J"]]
+
   const {id,date}=useParams()
-  const [selectedSeats,setSelectedSeats]=useState()
-  const [selectedTime,setSelectedTime]=useState()
+  const [selectedSeats,setSelectedSeats]=useState([])
+  const [selectedTime,setSelectedTime]=useState(null)
   const [show,setShow]=useState(null)
 
   const navigate=useNavigate()
@@ -23,6 +28,35 @@ const SeatLayout = () => {
       })
     }
   }
+
+  const handleSeatClick=(seatId)=>{
+    if(!selectedTime){
+      return toast("Please Select Time First")
+    }
+    if(!selectedSeats.includes(seatId) && selectedSeats.length>4){
+      return toast("You can only select 5 seats")
+    }
+    setSelectedSeats(prev=>prev.includes(seatId)?prev.filter(seat=>seat!==seatId):[...prev,seatId])
+  }
+
+  const renderSeats=(row,count=9)=>(
+    <div key={row} className='flex gap-2 mt-2' >
+      <div className='flex flex-wrap items-center justify-center gap-2'>
+        {
+          Array.from({length:count},(_,i)=>{
+            const seatId=`${row}${i+1}`;
+            return(
+              <button key={seatId} onClick={()=>handleSeatClick
+                (seatId)} className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) &&
+                  "bg-primary text-white"}`} >{seatId}</button>
+            );
+          })
+        }
+
+      </div>
+
+    </div>
+  )
 
   useEffect(()=>{
     getShow()
@@ -50,6 +84,14 @@ const SeatLayout = () => {
         <h1 className='text-2xl font-semibold mb-4'>Select your seat</h1>
         <img src={assets.screenImage} alt="screen"/>
         <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
+
+        <div className='flex flex-col items-center mt-10 text-xs text-gray-300'>
+
+          <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
+            {groupRows[0].map(row=>renderSeats(row))}
+          </div>
+        </div>
+
 
       </div>
 
